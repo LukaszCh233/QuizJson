2fdfdbdc045178c62c1de2748c9c3932ca933b7c
LukaszCh233 <103947294+LukaszCh233@users.noreply.github.com>
2023-01-31 15:23:28 +0100

    Add files via upload

--- JSON.java
+++ JSON.java
@@ -1,48 +1,24 @@
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.io.File;
 import java.util.*;
 public class JSON {
-
     public static void main(String[] args) throws IOException {
+
         Scanner scanner = new Scanner(System.in);
-        String odp;
-        int pkt = 0;
-        Random random = new Random();
-        int randomQuestion = 0;
-        ObjectMapper mapper = new ObjectMapper();
-        Question[] question = mapper.readValue(new File("C:\\Users\\chwie\\IdeaProjects\\untitled\\.idea\\vertopal.com_jsonformatter (3).json"), Question[].class);
-
-        ArrayList<Integer> drawnQuestion = new ArrayList<>();
-
-        for (int i = 0; i < question.length; i++) {
-
-            boolean drawnUniQuestion = false;
-            while (!drawnUniQuestion) {
-                randomQuestion = random.nextInt(question.length);
-
-                if (!drawnQuestion.contains(randomQuestion)) {
-                    drawnUniQuestion = true;
-                    drawnQuestion.add(randomQuestion);
-                }
-            }
-            System.out.println(question[randomQuestion].getQuestion1());
-            Object [] aa = question[randomQuestion].getAnswer1().toArray();
-            for (int y = 0; y < aa.length; y++){
-                System.out.println(aa[y]);
-            }
-
-            odp = scanner.nextLine();
-            if (odp.equalsIgnoreCase(question[randomQuestion].getCorrectAnswer())) {
-                pkt++;
-                System.out.println("Correct answer");
-            } else System.out.println("Bad answer");
+        String choice;
+        System.out.println("WITAJ W APLIKACJI QUIZ :)");
+        System.out.println("""
+                WYBIERZ JAKI QUIZ CHCESZ ROZWIĄZAĆ:
+                1 - Genetyka
+                2 - Fiziologia (Układ pokarmowy)""");
+        choice = scanner.nextLine();
+        if (choice.equals("1")) {
+            System.out.println("Wybrałeś Quiz o genetyce\nZACZYNAJMY :)");
+            QuizChoice.choiceQuiz();
+        } else if (choice.equals("2")) {
+            System.out.println("Wybrałeś Quiz o układzie pokarmowym\nZACZYNAJMY :)");
+            QuizChoice.choiceQuiz1();
         }
-        int questionsLength = question.length;
-        double wynik = (double) pkt / questionsLength;
-        System.out.println("Your score: " + wynik * 100 +"%");
+        else System.out.println("NIESTETY PODAŁEŚ ZŁY NUMER ZESTAWU");
     }
 }
-
-
---
+++ QuizChoice.java
@@ -0,0 +1,92 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class QuizChoice  {
+    public static void choiceQuiz ()  throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String odp;
+        int pkt = 0;
+        Random random = new Random();
+        int randomQuestion = 0;
+        ObjectMapper mapper = new ObjectMapper();
+        Question[] question = mapper.readValue(new File("C:\\Users\\chwie\\IdeaProjects\\untitled\\.idea\\vertopal.com_jsonformatter (3).json"), Question[].class);
+        ArrayList<Integer> drawnQuestion = new ArrayList<>();
+
+        for (int i = 0; i < question.length; i++) {
+
+            boolean drawnUniQuestion = false;
+            while (!drawnUniQuestion) {
+                randomQuestion = random.nextInt(question.length);
+
+                if (!drawnQuestion.contains(randomQuestion)) {
+                    drawnUniQuestion = true;
+                    drawnQuestion.add(randomQuestion);
+                }
+            }
+            System.out.println(question[randomQuestion].getQuestion1());
+            Object [] aa = question[randomQuestion].getAnswer1().toArray();
+            for (int y = 0; y < aa.length; y++){
+                System.out.println(aa[y]);
+            }
+            System.out.println("\nAnswer: ");
+            odp = scanner.nextLine();
+            if (odp.equalsIgnoreCase(question[randomQuestion].getCorrectAnswer())) {
+                pkt++;
+                System.out.println("Correct answer");
+            } else System.out.println("Bad answer");
+        }
+        int questionsLength = question.length;
+        double wynik = (double) pkt / questionsLength;
+        System.out.println("Your score: " + wynik * 100 +"%");
+    }
+    public static void choiceQuiz1 () throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String odp;
+
+        int pkt = 0;
+        Random random = new Random();
+        int randomQuestion = 0;
+        ObjectMapper mapper = new ObjectMapper();
+        Question[] question = mapper.readValue(new File("C:\\Users\\chwie\\IdeaProjects\\untitled\\.idea\\vertopal.com_jsonformatter (3).json"), Question[].class);
+        ArrayList<Integer> drawnQuestion = new ArrayList<>();
+
+        for (int i = 0; i < question.length; i++) {
+
+            boolean drawnUniQuestion = false;
+            while (!drawnUniQuestion) {
+                randomQuestion = random.nextInt(question.length);
+
+                if (!drawnQuestion.contains(randomQuestion)) {
+                    drawnUniQuestion = true;
+                    drawnQuestion.add(randomQuestion);
+                }
+            }
+            System.out.println(question[randomQuestion].getQuestion1());
+            Object [] aa = question[randomQuestion].getAnswer1().toArray();
+            for (int y = 0; y < aa.length; y++){
+                System.out.println(aa[y]);
+            }
+            System.out.println("\nAnswer: ");
+            odp = scanner.nextLine();
+            if (odp.equalsIgnoreCase(question[randomQuestion].getCorrectAnswer())) {
+                pkt++;
+                System.out.println("Correct answer");
+            } else System.out.println("Bad answer");
+        }
+        int questionsLength = question.length;
+        double wynik = (double) pkt / questionsLength;
+        System.out.println("Your score: " + wynik * 100 +"%");
+    }
+}
+
+
+
+
+
+
+
