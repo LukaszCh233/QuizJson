fb10b9a85389ae259dc616554ebbf95f2b7abeb2
LukaszCh233 <103947294+LukaszCh233@users.noreply.github.com>
2023-02-09 12:00:28 +0100

    Add files via upload

--- JSON.java
+++ JSON.java
@@ -1,8 +1,13 @@
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 public class JSON {
     public static void main(String[] args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
 
         Scanner scanner = new Scanner(System.in);
         String choice;
@@ -12,13 +17,18 @@ public class JSON {
                 1 - Genetyka
                 2 - Fiziologia (Układ pokarmowy)""");
         choice = scanner.nextLine();
+       Question [] question = new Question[0];
+
         if (choice.equals("1")) {
+            question= mapper.readValue(new File("vertopal.com_jsonformatter (3).json"),Question[].class);
             System.out.println("Wybrałeś Quiz o genetyce\nZACZYNAJMY :)");
-            QuizChoice.choiceQuiz();
+
         } else if (choice.equals("2")) {
+            question = mapper.readValue(new File("vertopal.com_jsonformatter (4).json"),Question[].class);
             System.out.println("Wybrałeś Quiz o układzie pokarmowym\nZACZYNAJMY :)");
-            QuizChoice.choiceQuiz1();
+
         }
         else System.out.println("NIESTETY PODAŁEŚ ZŁY NUMER ZESTAWU");
+        QuizChoice.choiceQuiz1(question);
     }
 }
